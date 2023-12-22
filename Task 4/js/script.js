e4691aa32651d06bc2a86f2fefdea758ff9ce099
Soumendu <118378045+Soumendu22@users.noreply.github.com>
2023-12-22 12:31:21 +0530

    Add files via upload

--- Task 4/js/script.js
+++ Task 4/js/script.js
@@ -1,9 +1,7 @@
-// Get the button:
 let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
+window.onscroll = function () {
+  scrollFunction();
+};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
@@ -11,20 +9,16 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
-
-function displayInfo(){
+function displayInfo() {
   alert("Form Submitted Succesfully,I will reach to you as soon as possible.");
 }
-
-let output = document.getElementById('submit');
-const formToReset = document.getElementById('form');
-formToReset.addEventListener('submit', (e) => {
-   e.preventDefault();
-   formToReset.reset();
+let output = document.getElementById("submit");
+const formToReset = document.getElementById("form");
+formToReset.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formToReset.reset();
 });
